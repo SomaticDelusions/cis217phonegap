86d763b5c3dd77d6e5a7f15ccb173341f5b35408
Sean Glover <sglover42@gmail.com>
2013-06-12 10:14:21 -0700

    Initial Commit

---
+++ phonegap/buy.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Glover
+ * Date: 5/24/13
+ * Time: 1:30 PM
+ * To change this template use File | Settings | File Templates.
+ */
+//
+//print_r($_GET);
+//$qty = $_GET['qty'];
+//$pids = $_GET['pid'];
+//
+//print_r($qty);
+//print_r($pids);
+
+    $form =  <<<END_OF_FORM
+
+    <form method="post" action="purchase.php?">
+        Name: <input type="text" name="name"><br /><br/>
+        Address: <input type="address" name="address"><br /><br/>
+        <input type="submit" name="submit" value="Submit Order" />
+    </form>
+END_OF_FORM;
+
+    echo json_encode($form);
+
+
